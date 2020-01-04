78a07bcf98857c6f8d1dc1322fd81c2defdaa335
fabio <carbonefabio@live.com>
2020-01-04 16:16:31 +0100

    mod5

--- src/rich-responses/card-response.js
+++ src/rich-responses/card-response.js
@@ -42,18 +42,26 @@ class Card extends RichResponse {
    *     title: 'card title',
    *     text: 'card text',
    *     imageUrl: https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png,
-   *     buttonText: This is a button',
-   *     buttonUrl: 'https://assistant.google.com/',
+   *     
    *     platform: 'ACTIONS_ON_GOOGLE',
-
-         buttonText1: '',
-         buttonUrl1: '',
+   *     
+   *     buttonText1: 'This is a button',
+   *     buttonUrl1: 'https://assistant.google.com/',
+   *
+   *     buttonText2: 'This is a button',,
+   *     buttonUrl2: 'https://assistant.google.com/',
+   *
+   *     buttonText3: 'This is a button',,
+   *     buttonUrl3: 'https://assistant.google.com/',
    * });
    *
    * @param {string|Object} card response title string or an object representing a card response
    */
   constructor(card) {
     super();
+
+    console.log("creating card v1");
+
     if (card === undefined || (typeof card === 'object' && !card.title)) {
       throw new Error('title string required by Card constructor');
     }
@@ -63,17 +71,22 @@ class Card extends RichResponse {
       this.title = card.title;
       this.text = card.text;
       this.imageUrl = card.imageUrl;
+      /*
       if (
         (!card.buttonText && card.buttonUrl) ||
         (!card.buttonUrl && card.buttonText)
       ) {
         throw new Error('card button requires both title and link');
       }
-      this.buttonText = card.buttonText;
-      this.buttonUrl = card.buttonUrl;
+      */
+      this.buttonText1 = card.buttonText1;
+      this.buttonUrl1 = card.buttonUrl1;
 
-      this.buttonText1 = card.buttonText1; //f-C
-      this.buttonUrl1 = card.buttonUrl1; //f-C
+      this.buttonText2 = card.buttonText2;
+      this.buttonUrl2 = card.buttonUrl2;
+
+      this.buttonText3 = card.buttonText3;
+      this.buttonUrl3 = card.buttonUrl3;
 
       if (
         typeof card.platform !== 'undefined' &&
@@ -278,37 +291,23 @@ class Card extends RichResponse {
       response.card.title = this.title;
       if (this.text) response.card.subtitle = this.text;
       if (this.imageUrl) response.card.imageUri = this.imageUrl;
-       
-      
-      
+
       response.card.buttons = [];
-      response.card.buttons[0] = {};
-      response.card.buttons[0].text = "test3";
-      response.card.buttons[0].postback = "test3";
-      
-      response.card.buttons[1] = {};
-      response.card.buttons[1].text = "test3";
-      response.card.buttons[1].postback = "test3";
-      
-      response.card.buttons[2] = {};
-      response.card.buttons[2].text = "test3";
-      response.card.buttons[2].postback = "test3";
-      
-      /*
-      if (this.buttonText && this.buttonUrl) {
-        console.log("adding button0");
-        response.card.buttons = [];
-        response.card.buttons[0] = {};
-        response.card.buttons[0].text = this.buttonText;
-        response.card.buttons[0].postback = this.buttonUrl;
-      }
       if (this.buttonText1 && this.buttonUrl1) {
-        console.log("adding button1");
-        response.card.buttons[1] = {};
-        response.card.buttons[1].text = this.buttonText1;
-        response.card.buttons[1].postback = this.buttonUrl1;
+        response.card.buttons[0] = {};
+        response.card.buttons[0].text = this.buttonText1;
+        response.card.buttons[0].postback = this.buttonUrl1;
       }
-      */
+      if (this.buttonText2 && this.buttonUrl2) {
+        response.card.buttons[1] = {};
+        response.card.buttons[1].text = this.buttonText2;
+        response.card.buttons[1].postback = this.buttonUrl2;
+      }
+      if (this.buttonText3 && this.buttonUrl3) {
+        response.card.buttons[2] = {};
+        response.card.buttons[2].text = this.buttonText3;
+        response.card.buttons[2].postback = this.buttonUrl3;
+      }
       
       // response is the same for generic responses without the platform attribute
       // if the platform is not undefined or the platform is not unspecified
